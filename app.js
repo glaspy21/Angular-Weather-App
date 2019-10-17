681a57c71727d2c3f18ae04b0bbdfd4d1f2b9766
Byron Glaspy <byronglaspy@Byrons-MacBook-Pro.local>
2019-10-17 17:52:12 -0400

    Setup Basic Pages

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const weatherApp = angular.module('weatherApp', []);
+const weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+weatherApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        
+        .when('/forecast', {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        })
+})
+
+
+weatherApp.controller('homeController', ['$scope', function($scope) {
+    $scope.name = 'Home'
+    
+}])
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+    
+}])
