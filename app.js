655aabce8a4a857104342b433fb05be1c891a361
Byron Glaspy <byronglaspy@byrons-mbp.attlocal.net>
2019-10-19 19:55:23 -0400

    setup api get with

--- app.js
+++ app.js
@@ -13,13 +13,16 @@ weatherApp.config(function($routeProvider) {
         })
 })
 
+//SERVICES
 weatherApp.service('cityService', function() {
-    this.city = "New York, NY";
-}) 
+    this.city = 'Miami ,US'
+})
 
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 
-    $scope.city = cityService.city;
+//CONTROLLERS
+weatherApp.controller('homeController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+
+    $scope.city = cityService.city
 
     $scope.$watch('city', function() {
         cityService.city = $scope.city
@@ -27,8 +30,13 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     
 }])
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource,cityService) {
 
-    $scope.city = cityService.city;
+    $scope.city = cityService.city    
+
+    $scope.weatherAPI = $resource('https://api.openweathermap.org/data/2.5/forecast', {callback: "JSON_CALLBACK", appid: '09fbb097e981c73f49e4b1014b726b1a' }, { get: { method: "JSONP" }});
+
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city})
     
-}])
+    console.log($scope.weatherResult)
+}]) 
